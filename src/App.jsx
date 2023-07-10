508ad9652de8e731c65e34d7646534ea256696fa
Arekkusu17 <fernn.alex@gmail.com>
2023-07-10 17:30:13 -0400

    add Route for each dashboard path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Container } from "@mui/material"
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/navbar/Navbar"
+import Dashboard from "./pages/Dashboard"
+import Profile from "./pages/Profile"
+import EditProfile from "./pages/EditProfile"
+import Favorites from "./pages/Favorites"
+import CreatePost from "./pages/CreatePost"
+import MyPosts from "./pages/MyPosts"
 
 export default function App() {
   return (
-
     <>
       <Navbar />
       <Container>
@@ -15,10 +20,15 @@ export default function App() {
           <Route path="/register"></Route>
           <Route path="/profile"></Route>
           <Route path="/cart"></Route>
-          <Route path="#"></Route>
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="profile" element={<Profile />} />
+            <Route path="editprofile" element={<EditProfile />} />
+            <Route path="favorites" element={<Favorites />} />
+            <Route path="createpost" element={<CreatePost />} />
+            <Route path="posts" element={<MyPosts />} />
+          </Route>
         </Routes>
       </Container>
     </>
-
   )
 }
